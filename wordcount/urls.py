b1835129d78c86863ec098a107678aa88a839ada
harjeevansingh <harjeevansingh01@gmail.com>
2018-12-02 22:41:11 +0530

    added about page

--- wordcount/urls.py
+++ wordcount/urls.py
@@ -3,9 +3,9 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-	path('', views.homepage, name='home'),
-	path('count/', views.count, name='count'),
-	path('about/', views.about, name='about'),
+	path('admin/', admin.site.urls),
+	path('', views.homepage, name='home'),			# Home Page
+	path('count/', views.count, name='count'),   # Count page
+	path('about/', views.about, name='about'),		# About Page
 
 ]
